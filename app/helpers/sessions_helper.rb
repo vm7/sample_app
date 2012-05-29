f8c29fa1e6cf8457632e2ba637d4bc66fa291545
vm <vedranmoric@yahoo.com>
2012-05-29 10:54:35 +0200

    sesssion umjesto cookia

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,24 +3,38 @@ module SessionsHelper
   #radi sign in usera
   #permanent cookies<---signed
   #poziva se u session controleru
+  #def sign_in(user)
+  # cookies.permanent.signed[:remember_token] = [user.id, user.salt]
+  #current_user = user
+  #end
   def sign_in(user)
-    cookies.permanent.signed[:remember_token] = [user.id, user.salt]
-    current_user = user
+    #cookies.permanent.signed[:remember_token] = [user.id, user.salt]
+    session[:user_id] = user.id
+    self.current_user = user
   end
+
+
 
   def current_user=(user)
     @current_user = user
   end
 
-  #poziva se vise puta session perssits
   def current_user
-    #ako je user nill assignat ce mu usera sa remeber talkena,ako  je tru
-    @current_user ||= user_from_remember_token
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  #@current_user ||= user_from_remember_token
   end
+
+  #poziva se vise puta session perssits
+  #def current_user
+  #ako je user nill assignat ce mu usera sa remeber talkena,ako  je tru
+  # @current_user ||= user_from_remember_token
+  #end
 
   def signed_in?
     !current_user.nil?
   end
+  
+
 
   def signed_out
     self.current_user=nil
@@ -28,21 +42,28 @@ module SessionsHelper
 
   #poziva se u session controleru
   #sign out usera
+  #def sign_out
+  #cookies.delete(:remember_token)
+  #current_user = nil
+  #end
 
+  #poziva se u session controleru
   def sign_out
-    cookies.delete(:remember_token)
-    current_user = nil
+    #cookies.delete(:remember_token)
+    session[:user_id] = nil
+    self.current_user = nil
   end
-
+  
+  
   private
 
   #autenticira usera sa saltom i proslsjedjue
-  def user_from_remember_token
-    User.authenticate_with_salt(*remember_token)
-  end
+  #def user_from_remember_token
+   # User.authenticate_with_salt(*remember_token)
+  #end
 
-  def remember_token
-    cookies.signed[:remember_token] || [nil, nil]
-  end
+  #def remember_token
+   # cookies.signed[:remember_token] || [nil, nil]
+  #end
 
 end
